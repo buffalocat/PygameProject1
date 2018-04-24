167ea8acd7310fa95c09fd50562956a629cf2c99
buffalocat <buffalocat@users.noreply.github.com>
2018-04-23 23:49:58 -0700

    Changed the .map file format
    It should be more robust now.  It's slightly weird in that what gets imported isn't exactly what gets exported; as it is now, the game "forgets" what classes objects were, and just remembers their properties.  That said, this doesn't cause any concrete problems.
    
    The example levels haven't been significantly changed, but they were moved to the new format.

--- gs/sokoban.py
+++ gs/sokoban.py
@@ -3,7 +3,7 @@ from queue import Queue
 from tkinter import filedialog
 
 import pygame
-from enum import IntEnum
+from enum import IntEnum, Enum
 
 from pygame.rect import Rect
 
@@ -25,20 +25,19 @@ LINE_WIDTH = 1
 
 DIR = {K_RIGHT: (1, 0), K_DOWN: (0, 1), K_LEFT: (-1, 0), K_UP: (0, -1)}
 
-# Later we'll want a more robust method for storing level data...
+ADJ = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+DEBUG_GROUPS = False
+
+# We can probably do something very "clever" here to avoid retyping words...
+class Attr(Enum):
+    solid = b"solid"
+    pushable = b"pushable"
+    sticky = b"sticky"
 
 class Tile(IntEnum):
     EMPTY = 0
     WALL = 1
-    BOX = 2
-    PLAYER = 3
-    STICKY = 4
-    PURPLE = 5
-
-ADJ = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
-VERBOSE = False
-
 
 class GSSokoban(GameState):
     def __init__(self, mgr, parent):
@@ -48,7 +47,7 @@ class GSSokoban(GameState):
         self.bg = WHITE
         self.create_mode = Wall
         self.set_sample()
-        self.room_load_default()
+        self.load()
         self.create_text()
 
     def draw(self):
@@ -63,8 +62,7 @@ class GSSokoban(GameState):
             if event.type == KEYDOWN:
                 if event.key in DIR.keys():
                     if self.try_move(DIR[event.key], self.player):
-                        self.room[self.player.pos] = self.player
-                    break
+                        break
                 elif event.key in CREATE.keys():
                     self.create_mode = CREATE[event.key]
                     self.set_sample()
@@ -84,23 +82,18 @@ class GSSokoban(GameState):
                     if pos != self.player.pos:
                         self.destroy(pos)
 
-    def create(self, pos, type=None):
-        if self.room[pos] is not None:
+    def create(self, pos):
+        obj = self.create_mode(pos)
+        if obj.solid and self.get_solid(pos) is not None:
             return False
-        if type is None:
-            obj = self.create_mode(pos)
-        else:
-            obj = type(pos)
-        self.room[pos] = obj
+        self.put_obj(obj, pos)
         if obj.sticky:
             self.update_group(obj)
-        if VERBOSE:
-            self.print_objs()
         return True
 
     def destroy(self, pos):
-        obj = self.room[pos]
-        self.room[pos] = None
+        obj = self.get_solid(pos)
+        self.objmap[pos] = []
         # Reevaluate stickiness relations now that obj is "gone"
         if obj is not None and obj.sticky:
             group = obj.root.group - {obj}
@@ -117,19 +110,25 @@ class GSSokoban(GameState):
     def create_wall_border(self):
         for y in [-1, self.h]:
             for x in range(self.w):
-                self.room[(x, y)] = Wall((x, y))
+                self.objmap[(x, y)] = [Wall((x, y))]
         for x in [-1, self.w]:
             for y in range(self.h):
-                self.room[(x, y)] = Wall((x, y))
+                self.objmap[(x, y)] = [Wall((x, y))]
 
     def set_sample(self):
         # An "object" at a fake position, not recorded on the grid
         self.sample = self.create_mode((-1, -1))
 
-    def try_move(self, dpos, entity):
+    def get_solid(self, pos):
+        """Return the solid object at pos, if it exists"""
+        if pos in self.objmap:
+            return next((obj for obj in self.objmap[pos] if obj.solid), None)
+        return None
+
+    def try_move(self, dpos, obj):
         # The set of all groups influenced by the motion
-        seen = {entity.root}
-        to_check = [entity.root]
+        seen = {obj.root}
+        to_check = [obj.root]
         can_move = True
         dx, dy = dpos
         while can_move and to_check:
@@ -137,44 +136,41 @@ class GSSokoban(GameState):
             for cur in to_check.pop().group:
                 x, y = cur.pos
                 # adj is the tile that cur is trying to move into
-                adj = self.room[(x+dx, y+dy)]
+                adj = self.get_solid((x+dx, y+dy))
+                # There was no solid object there
                 if adj is None:
                     continue
-                # Tell the adjacent object that something is
-                # trying to move into it
-                adj.replace_with_none = False
+                # There is a solid object, and we're already considering it
                 if adj.root in seen:
                     continue
+                # It's new, but we can push it
                 elif adj.pushable:
                     seen.add(adj.root)
                     to_check.append(adj.root)
+                # We're trying to push something we can't push
                 else:
-                    # We're trying to push something we can't push
                     can_move = False
                     break
         if can_move:
             for tile in seen:
                 for obj in list(tile.group):
+                    self.objmap[obj.pos].remove(obj)
                     x, y = obj.pos
-                    if obj.replace_with_none:
-                        self.room[obj.pos] = None
                     obj.pos = (x+dx, y+dy)
-                    self.room[obj.pos] = obj
+                    self.put_obj(obj, obj.pos)
+            for tile in seen:
+                for obj in list(tile.group):
                     if obj.sticky:
                         self.update_group(obj)
-        # In any case, reset the "replace" flags
-        for tile in seen:
-            for obj in tile.group:
-                obj.replace_with_none = True
-        if VERBOSE:
-            self.print_objs()
         return can_move
 
     def update_group(self, obj):
         x, y = obj.pos
         for dx, dy in ADJ:
-            adj = self.room[(x+dx, y+dy)]
+            adj = self.get_solid((x+dx, y+dy))
             if adj is not None and adj.sticky and obj.color == adj.color and obj.root is not adj.root:
+                if DEBUG_GROUPS:
+                    print(f"Merging {obj} with {adj}")
                 obj.merge_group(adj)
 
     def create_text(self):
@@ -185,14 +181,6 @@ class GSSokoban(GameState):
         self.text.add_line("Press S to Save and L to Load")
         self.text.add_line("ZXCVB change block type")
         self.text.add_line("Left/Right click to Create/Destroy")
-
-    def room_load_default(self):
-        self.w = ROOM_WIDTH
-        self.h = ROOM_HEIGHT
-        self.room = {(x, y): None for x in range(ROOM_WIDTH) for y in range(ROOM_HEIGHT)}
-        self.player = Player((2,2))
-        self.room[self.player.pos] = self.player
-        self.create_wall_border()
 
     @staticmethod
     def real_pos(x, y):
@@ -206,17 +194,18 @@ class GSSokoban(GameState):
         # This will need to be changed significantly
         # once we have a "camera" object
         pygame.draw.rect(self.surf, WHITE, Rect(PADDING, PADDING, MESH*ROOM_WIDTH, MESH*ROOM_HEIGHT))
-        for x in range(self.w):
-            for y in range(self.h):
-                tile = self.room[(x,y)]
-                if tile is not None:
-                    tile.draw(self.surf, self.real_pos(x, y))
+        for pos in self.objmap:
+            if self.in_bounds(pos):
+                x, y = pos
+                for obj in self.objmap[(x,y)]:
+                    obj.draw(self.surf, self.real_pos(x, y))
 
     def move_player(self, pos):
-        if self.player is not None:
-            self.room[self.player.pos] = None
-        self.player = Player(pos)
-        self.room[pos] = self.player
+        if self.player is None:
+            self.player = Player(pos)
+        else:
+            self.objmap[self.player.pos].remove(self.player)
+        self.objmap[pos].append(self.player)
 
     def save(self):
         filename = filedialog.asksaveasfilename(initialdir=MAIN_DIR, filetypes=["map .map"])
@@ -225,15 +214,42 @@ class GSSokoban(GameState):
         if filename[-4:] != ".map":
             filename += ".map"
         try:
-            with open(filename, "wb") as file:
+            with open(filename, "w+b") as file:
+                # IF NOT BIGMAP
                 file.write(bytes([self.w, self.h]))
-                tile_data = []
-                for y in range(self.h):
-                    for x in range(self.w):
-                        tile = self.room[(x,y)]
-                        code = 0 if tile is None else tile.type
-                        tile_data.append(code)
-                file.write(bytes(tile_data))
+                objdata = {}
+                for pos in self.objmap:
+                    if not self.in_bounds(pos):
+                        continue
+                    for obj in self.objmap[pos]:
+                        attrs = []
+                        if obj.solid:
+                            attrs.append(b"solid")
+                        if obj.pushable:
+                            attrs.append(b"pushable")
+                        if obj.sticky:
+                            attrs.append(b"sticky")
+                        if obj.is_player:
+                            attrs.append(b"is_player")
+                        color = bytes(obj.color)
+                        s = b" ".join(attrs) + b":" + color + b"\n"
+                        if s not in objdata:
+                            objdata[s] = []
+                        objdata[s].append(obj.pos)
+                for s in objdata:
+                    # We write:
+                    # 1) The object type, followed by newline
+                    # 2) The number of occurences
+                    # 3) The positions
+                    file.write(s)
+                    # IF NOT BIGMAP
+                    file.write(len(objdata[s]).to_bytes(2, byteorder="little"))
+                    pos_list = []
+                    for pos in objdata[s]:
+                        if self.in_bounds(pos):
+                            # IF NOT BIGMAP
+                            pos_list.extend(pos)
+                    file.write(bytes(pos_list))
         except IOError:
             print("Failed to write to file")
 
@@ -242,52 +258,66 @@ class GSSokoban(GameState):
         if filename is None:
             return
         try:
-            with open(filename, "rb") as file:
+            with open(filename, "r+b") as file:
+                # IF NOT BIGMAP
                 room_size = file.read(2)
                 self.w = int(room_size[0])
                 self.h = int(room_size[1])
-                self.room = {(x,y): None for x in range(self.w) for y in range(self.h)}
+                self.objmap = {}
                 self.player = None
                 x, y = 0, 0
-                tile_data = file.read(self.w * self.h)
-                for byte in tile_data:
-                    code = int(byte)
-                    # We're gonna do this the dumbest way possible, for now
-                    if code == Tile.WALL:
-                        self.create((x, y), type=Wall)
-                    elif code == Tile.BOX:
-                        self.create((x, y), type=Box)
-                    elif code == Tile.STICKY:
-                        self.create((x, y), type=StickyBox)
-                    elif code == Tile.PURPLE:
-                        self.create((x, y), type=PurpleBox)
-                    elif code == Tile.PLAYER:
-                        self.move_player((x,y))
-                    x += 1
-                    if x >= self.w:
-                        x = 0
-                        y += 1
                 self.create_wall_border()
+                for line in file:
+                    if len(line) <= 1:
+                        break
+                    spl = line.rstrip(b"\n").split(b":")
+                    # A list of attributes that the object has
+                    attr = spl[0].split(b" ")
+                    solid = b"solid" in attr
+                    pushable = b"pushable" in attr
+                    sticky = b"sticky" in attr
+                    is_player = b"is_player" in attr
+                    # The color of the object in rgb format
+                    color = tuple(map(int, spl[1]))
+                    # The positions at which this object appears
+                    # IF NOT BIGMAP
+                    n = int.from_bytes(file.read(2), byteorder="little")
+                    pos_str = file.read(2*n)
+                    # IF NOT BIGMAP
+                    for i in range(n):
+                        pos = tuple(map(int, pos_str[2*i:2*i + 2]))
+                        obj = Entity(pos)
+                        obj.solid = solid
+                        obj.pushable = pushable
+                        obj.sticky = sticky
+                        obj.color = color
+                        obj.is_player = is_player
+                        self.put_obj(obj, pos)
+                for pos in self.objmap:
+                    for obj in self.objmap[pos]:
+                        if obj.sticky:
+                            self.update_group(obj)
         except IOError:
             print("Failed to read file")
 
-    def print_objs(self):
-        for x in range(self.w):
-            for y in range(self.h):
-                o = self.room[(x, y)]
-                if o is not None:
-                    print(f"{o} with root {o.root}, in {o.group}")
-        print("")
+    def put_obj(self, obj, pos):
+        """Push obj into the objmap at pos"""
+        if obj.is_player:
+            self.player = obj
+        if pos not in self.objmap:
+            self.objmap[pos] = []
+        self.objmap[pos].append(obj)
 
 
 class Entity:
     ID_COUNT = 0
 
-    def __init__(self, pos):
+    def __init__(self, pos=None):
         self.pos = pos
+        self.solid = True
         self.pushable = False
         self.color = None
-        self.type = None
+        self.is_player = False
 
         self.sticky = False
         self.root = self
@@ -296,8 +326,6 @@ class Entity:
         self.id = Entity.ID_COUNT
         Entity.ID_COUNT += 1
 
-        # Just for keeping track of what happens when moving
-        self.replace_with_none = True
 
     def merge_group(self, obj):
         self.root.group |= obj.root.group
@@ -307,9 +335,12 @@ class Entity:
 
     def draw(self, surf, pos):
         pygame.draw.rect(surf, self.color, Rect(pos, (MESH, MESH)))
+        if self.is_player:
+            center = (pos[0] + MESH // 2, pos[1] + MESH // 2)
+            pygame.draw.circle(surf, BLACK, center, MESH // 4, 0)
 
     def __repr__(self):
-        return f"{self.id} at {self.pos}"
+        return f"Color {self.color} obj at Pos {self.pos}"
 
 
 class Player(Entity):
@@ -317,25 +348,13 @@ class Player(Entity):
         super().__init__(pos)
         self.pushable = True
         self.color = RED
-        self.type = Tile.PLAYER
-
-    def draw(self, surf, pos):
-        super().draw(surf, pos)
-        center = (pos[0] + MESH//2, pos[1] + MESH//2)
-        pygame.draw.circle(surf, BLACK, center, MESH//4, 0)
-
-    def __repr__(self):
-        return "Player " + super().__repr__()
+        self.is_player = True
 
 
 class Wall(Entity):
-    def __init__(self, pos):
+    def __init__(self, pos=None):
         super().__init__(pos)
         self.color = BLACK
-        self.type = Tile.WALL
-
-    def __repr__(self):
-        return "Wall " + super().__repr__()
 
 
 class Box(Entity):
@@ -343,10 +362,6 @@ class Box(Entity):
         super().__init__(pos)
         self.pushable = True
         self.color = GREEN
-        self.type = Tile.BOX
-
-    def __repr__(self):
-        return "Box " + super().__repr__()
 
 
 class StickyBox(Entity):
@@ -356,16 +371,11 @@ class StickyBox(Entity):
         self.grouped = True
         self.color = color
         self.sticky = True
-        self.type = Tile.STICKY
-
-    def __repr__(self):
-        return "StickyBox " + super().__repr__()
 
 
 class PurpleBox(StickyBox):
     def __init__(self, pos):
         super().__init__(pos, color=PURPLE)
-        self.type = Tile.PURPLE
 
 
 CREATE = {K_z: Wall, K_x: Box, K_c: Player, K_v: StickyBox, K_b: PurpleBox}
